6a37fd8d96ec1b52291ec02bc053a572b6b21ed0
see-clumsy <117338916+see-clumsy@users.noreply.github.com>
2022-11-03 19:28:13 +0800

    Update GitHubController.java

--- src/main/java/cn/bdqn/controller/GitHubController.java
+++ src/main/java/cn/bdqn/controller/GitHubController.java
@@ -9,6 +9,6 @@ package cn.bdqn.controller;
 public class GitHubController {
 
     public static void main(String[] args) {
-        System.out.println("黄天翔");
+        System.out.println("黄天");
     }
 }
