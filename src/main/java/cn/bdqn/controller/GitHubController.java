17ef3c7f7b11685b8e9cce6e1978d68df5dd9811
see-clumsy <778754458@qq.com>
2022-11-03 17:33:07 +0800

    更新GitHub访问接口第一次修改

---
+++ src/main/java/cn/bdqn/controller/GitHubController.java
@@ -0,0 +1,12 @@
+package cn.bdqn.controller;
+
+/**
+ * @ClassName: GitHubController
+ * @Description:
+ * @Date: 2022-11-03 17:23:40
+ * @Author: Pang YangGung
+ */
+public class GitHubController {
+
+
+}
